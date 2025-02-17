1600b764d38b3964a63712c14ece5a4794d63850
whos-soso <603811158@qq.com>
2025-02-17 14:24:58 +0800

    Update ekan.py

--- node_classification_grkan/ekan.py
+++ node_classification_grkan/ekan.py
@@ -12,25 +12,31 @@ class KANLinear(torch.nn.Module):
     def __init__(
             self,
             in_features,
-            out_features,
-            base_activation=dict(type="KAT", act_init=["gelu", "gelu"]),
+            hidden_features=None,
+            out_features=None,
+            act_cfg=dict(type="KAT", act_init=["identity", "gelu"]),
             bias=True,
             drop=0.,
     ):
         super().__init__()
         out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
 
-        self.fc1 = nn.Linear(in_features, out_features, bias=bias)
-        self.act1 = KAT_Group(mode = base_activation['act_init'][0])
+        self.fc1 = nn.Linear(in_features, hidden_features, bias=bias)
+        self.act1 = KAT_Group(mode = act_cfg['act_init'][0])
         self.drop1 = nn.Dropout(drop)
-
+        self.act2 = KAT_Group(mode = act_cfg['act_init'][1])
+        self.fc2 = nn.Linear(hidden_features, out_features, bias=bias)
+        self.drop2 = nn.Dropout(drop)
 
     def forward(self, x):
         x = self.act1(x)
         x = self.drop1(x)
         x = self.fc1(x)
+        x = self.act2(x)
+        x = self.drop2(x)
+        x = self.fc2(x)
         return x
-
     
 
 
