aae2e1651d3228cef11c338b26447b56c3fe517a
whos-soso <603811158@qq.com>
2025-02-17 19:54:55 +0800

    Update utils.py

--- node_classification_clean/utils.py
+++ node_classification_clean/utils.py
@@ -160,7 +160,7 @@ def efficient_evaluation_loss(y, out, mask, criterion):
         return loss
 
 def train_total(model, params, data, train_mask, val_mask, test_mask=None):
-    torch.save(model, f"models_saves/{params['dataset']}_{params['architecture']}_{params['conv_type']}")
+    torch.save(model, f"KAGNN/node_classification_clean/models_saves/{params['dataset']}_{params['architecture']}_{params['conv_type']}")
     if test_mask is None:
         test_mask = val_mask
     early_stopper = EarlyStopper(patience=params['patience'])
@@ -179,11 +179,11 @@ def train_total(model, params, data, train_mask, val_mask, test_mask=None):
                 print(f'Train acc: {train_acc}, Val acc: {val_acc}, Test acc: {test_acc}')
         should_save, should_stop = early_stopper.early_stop(val_loss)
         if should_save:
-            torch.save(model, f"models_saves/{params['dataset']}_{params['architecture']}_{params['conv_type']}")
+            torch.save(model, f"KAGNN/node_classification_clean/models_saves/{params['dataset']}_{params['architecture']}_{params['conv_type']}")
         if should_stop:
             break
     print("load")
-    model = torch.load(f"models_saves/{params['dataset']}_{params['architecture']}_{params['conv_type']}")
+    model = torch.load(f"KAGNN/node_classification_clean/models_saves/{params['dataset']}_{params['architecture']}_{params['conv_type']}")
     train_acc = efficient_evaluation_accuracy(data.y, out, train_mask)
     val_acc = efficient_evaluation_accuracy(data.y, out, val_mask)
     test_acc = efficient_evaluation_accuracy(data.y, out, test_mask)
@@ -212,7 +212,7 @@ def all_splits(params, data):
 def run_experiment(params, data_name):
     print("Testing params")
     print(params)
-    log_file = f"logs/{data_name}_{params['architecture']}_{params['conv_type']}"
+    log_file = f"KAGNN/node_classification_clean/logs/{data_name}_{params['architecture']}_{params['conv_type']}"
     data = load_data(data_name)
     params['mp_layers'] = dataset_layers[data_name]
     params['num_classes'] = data.num_classes
